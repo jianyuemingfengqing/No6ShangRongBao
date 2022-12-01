76e1350164bcb55ee80f4916864e2308fedcb4bf
lin <link.twilght.gu@gmail.com>
2022-12-01 15:49:00 +0800

    由于没有添加注解, 导致自动填充日期用不了

--- service-core/src/main/java/com/learn/srb/core/handle/GlobalMetaObjectHandler.java
+++ service-core/src/main/java/com/learn/srb/core/handle/GlobalMetaObjectHandler.java
@@ -2,9 +2,10 @@ package com.learn.srb.core.handle;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
-
+@Component
 public class GlobalMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
